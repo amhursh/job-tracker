bc88bcb160c03c2c8800748281a9b752f37da6fd
amhursh <amhursh@gmail.com>
2017-09-03 00:52:51 -0600

    Add category validations and pass model tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,13 +4,14 @@ RSpec.describe Category, type: :model do
   describe "validations" do
     context "invalid attibutes" do
       it "is invalid without a title" do
-        category = build(:category)
+        category = build(:category, title: nil)
         expect(category).to be_invalid
       end
 
       it "is invalid without a unique title" do
         create(:category, title: "Yo")
         category = build(:category, title: "Yo")
+        expect(category).to be_invalid
       end
     end
 
