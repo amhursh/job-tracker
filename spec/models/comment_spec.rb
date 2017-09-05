01b3426b87614a8da4ff07641da0406656b8a88e
amhursh <amhursh@gmail.com>
2017-09-04 21:11:17 -0600

    Add comment model test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without content" do
+        job = create(:job)
+        comment = job.comments.new()
+        expect(comment).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with content" do
+        job = create(:job)
+        comment = job.comments.new(content: "Hello There")
+        expect(comment).to be_valid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "belongs to job" do
+      comment = Comment.new(content: "Yo")
+      expect(comment).to respond_to(:job)
+    end
+  end
 end
