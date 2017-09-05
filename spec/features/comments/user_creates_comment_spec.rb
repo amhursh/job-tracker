5767cc0faa00d1e44e1f4e1b2660b9da92ec9c2a
amhursh <amhursh@gmail.com>
2017-09-04 21:00:46 -0600

    Add comment date and pass feature testing

--- spec/features/comments/user_creates_comment_spec.rb
+++ spec/features/comments/user_creates_comment_spec.rb
@@ -8,8 +8,11 @@ describe "User creates a new comment" do
     visit company_job_path(company, job)
     fill_in "comment[content]", with: "test comment xzy"
     click_on "Add Comment"
+    fill_in "comment[content]", with: "Yahoooooo"
+    click_on "Add Comment"
 
     expect(current_path).to eq company_job_path(company, job)
     expect(page).to have_content("test comment xzy")
+    expect(page).to have_content("Yahoooooo")
   end
 end
