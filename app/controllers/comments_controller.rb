45c216d017202d495e91f438ea645c95f3c6bdec
amhursh <amhursh@gmail.com>
2017-09-04 20:36:58 -0600

    Complete new comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+  def create
+    @company = Company.find(params[:company_id])
+    @job = Job.find(params[:job_id])
+    @comment = @job.comments.new(comment_params)
+    @comment.save
+
+    redirect_to company_job_path(@company, @job)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
 end
