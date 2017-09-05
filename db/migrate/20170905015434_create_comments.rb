e0b59a36cf0814d5776c7991b5cf9d9f1f97c749
amhursh <amhursh@gmail.com>
2017-09-04 19:55:20 -0600

    Add comment model and index on job_id foreign key

--- db/migrate/20170905015434_create_comments.rb
+++ db/migrate/20170905015434_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
       t.text :content
-      t.references :job, foreign_key: true
+      t.references :job, foreign_key: true, index: true
 
       t.timestamps
     end
